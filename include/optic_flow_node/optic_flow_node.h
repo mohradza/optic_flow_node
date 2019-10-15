24acf2d1ded885d2ea7747338203fcabf7f58101
mohradza <mioh0887@colorado.edu>
2019-10-15 11:22:50 -0600

    finalized tangential optic flow calculation. Needs to be tested

--- include/optic_flow_node/optic_flow_node.h
+++ include/optic_flow_node/optic_flow_node.h
@@ -10,7 +10,9 @@
 #if ((BOOST_VERSION / 100) % 1000) >= 53
 #include <boost/thread/lock_guard.hpp>
 #endif
+#include <boost/thread.hpp>
 #include <boost/circular_buffer.hpp>
+#include <std_msgs/Float32MultiArray.h>
 
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/video/tracking.hpp>
@@ -18,6 +20,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <image_transport/image_transport.h>
+#include <optic_flow_node/OpticFlowNodeConfig.h>
+
 
 using namespace cv_bridge;
 using namespace cv;
@@ -34,19 +38,33 @@ class OpticFlowNode{
         void init();
 
         // FUNCTIONS
-        //void configCb(Config &config, uint32_t level)
         void imageCb(const sensor_msgs::ImageConstPtr& image_msg);
+
 
     private:
         ros::NodeHandle nh_;
         ros::NodeHandle pnh_;
         std::string node_name_{"node_name"};
 
-        ros::Subscriber image_sub_;
+        boost::mutex connect_mutex_;
+        boost::recursive_mutex config_mutex_;
+        typedef optic_flow_node::OpticFlowNodeConfig Config;
+        typedef dynamic_reconfigure::Server<Config> ReconfigureServer;
+        boost::shared_ptr<ReconfigureServer> reconfigure_server_;
+        Config config_;
+        void configCb(Config &config, uint32_t level);
+
+        ros::Subscriber sub_image_;
+        ros::Publisher pub_tang_flow_;
+
         std::string OPENCV_WINDOW;
         bool debug_;
         std::vector<Point2f> points2track_;
         std::vector<Point2f> newpoints_;
+        std::vector<float> u_flow_;
+        std::vector<float> v_flow_;
+        std::vector<float> tang_flow_;
+        std::vector<float> ave_tang_flow_;
         std::vector<float> gamma_vector_;
 
         int image_center_x_;
@@ -57,7 +75,12 @@ class OpticFlowNode{
         int ring_dr_;
         Mat prev_grey_image_;
         bool init_;
+        int win_size_;
+        double pixel_scale_;
+        float dt_;
 
+        ros::Time image_timestamp_;
+        ros::Time last_image_timestamp_;
 
 };
 
--- src/wide_field_optic_flow.cpp
+++ src/wide_field_optic_flow.cpp
@@ -11,50 +11,32 @@ OpticFlowNode::OpticFlowNode(const ros::NodeHandle &node_handle,
 
 void OpticFlowNode::init() {
     // Set up dynamic reconfigure
-    /*
+
     reconfigure_server_.reset(new ReconfigureServer(config_mutex_, pnh_));
     ReconfigureServer::CallbackType f = boost::bind(&OpticFlowNode::configCb, this, _1, _2);
     reconfigure_server_->setCallback(f);
-    */
-    debug_ = true;
+
     init_ = true;
     // Set up subscribers and callbacks
-    image_sub_ = nh_.subscribe("/usb_cam/image_raw", 1, &OpticFlowNode::imageCb, this);
+    sub_image_ = nh_.subscribe("/usb_cam/image_raw", 1, &OpticFlowNode::imageCb, this);
 
     // Set up publishers
-    //pub_h_scan_reformat_ = nh_.advertise<std_msgs::Float32MultiArray>("horiz_depth_reformat", 10);
-
+    pub_tang_flow_ = nh_.advertise<std_msgs::Float32MultiArray>("tang_optic_flow", 10);
 
     // Import parameters
-    nh_.param("/optic_flow_node/image_center_x", image_center_x_, 320);
-    nh_.param("/optic_flow_node/image_center_y", image_center_y_, 240);
+    nh_.param("/optic_flow_node/image_center_x", image_center_x_, 325);
+    nh_.param("/optic_flow_node/image_center_y", image_center_y_, 220);
     nh_.param("/optic_flow_node/inner_ring_radius", inner_ring_radius_, 150.0);
     nh_.param("/optic_flow_node/num_ring_points", num_ring_points_, 30);
     nh_.param("/optic_flow_node/num_rings", num_rings_, 1);
     nh_.param("/optic_flow_node/ring_dr", ring_dr_, 5);
 
-    //ROS_INFO("%d", total_h_scan_points_);
+    // LK parameters
+    nh_.param("/optic_flow_node/pyr_window_size", win_size_, 30);
+    nh_.param("/optic_flow_node/pixel_scale", pixel_scale_, 15.0);
 
-    // Generate the gamma vector
-    for(int i = 0; i < num_ring_points_; i++){
-        gamma_vector_.push_back((float(i)/float(num_ring_points_-1))*2*M_PI - M_PI);
-    }
-
-    // Figure out which points we want to track
-    int x = 1;
-    int y = 2;
-    //std::vector<Point2f> points;
-    points2track_.push_back(Point2f((float)image_center_x_, (float)image_center_y_));
-    cout << points2track_ << std::endl;
-    for(int r = 0; r < num_rings_; r++){
-        for(int i = 0; i < num_ring_points_; i++){
-           int x = image_center_x_ + int((inner_ring_radius_+float(r)*ring_dr_)*sin(gamma_vector_[i]));
-           int y =  image_center_y_ - int((inner_ring_radius_+float(r)*ring_dr_)*cos(gamma_vector_[i]));
-           points2track_.push_back(Point2f((float)x, (float)y));
-           cout << points2track_[r*num_ring_points_ + i] << std::endl;
-        }
-    }
-    newpoints_ = points2track_;
+    // Switches
+    nh_.param("/optic_flow_node/enable_debug", debug_, true);
 
     // Set up an opencv window for debugging
     OPENCV_WINDOW = "Image Window";
@@ -65,17 +47,40 @@ void OpticFlowNode::init() {
 
 } // End of init
 
-//void NearnessController::configCb(Config &config, uint32_t level)
-//{
-//    config_ = config;
+void OpticFlowNode::configCb(Config &config, uint32_t level)
+{
+    config_ = config;
 
-    // Controller gains
-    //u_k_hb_1_ = config_.forward_speed_k_hb_1;
-//}
+    image_center_x_ = config.image_center_x;
+    image_center_y_ = config.image_center_y;
+    inner_ring_radius_ = config.inner_ring_radius;
+    ring_dr_ = config.ring_dr;
+    num_ring_points_ = config.num_ring_points;
+    num_rings_ = config.num_rings;
+    win_size_ = config.pyr_window_size;
+    pixel_scale_ = config.pixel_scale;
+
+    gamma_vector_.clear();
+    for(int i = 0; i < num_ring_points_; i++){
+        gamma_vector_.push_back((float(i)/float(num_ring_points_-1))*2*M_PI - M_PI);
+    }
+
+    points2track_.clear();
+    points2track_.push_back(Point2f((float)image_center_x_, (float)image_center_y_));
+    for(int r = 0; r < num_rings_; r++){
+        for(int i = 0; i < num_ring_points_; i++){
+           int x = image_center_x_ + int((inner_ring_radius_+float(r)*ring_dr_)*sin(gamma_vector_[i]));
+           int y =  image_center_y_ - int((inner_ring_radius_+float(r)*ring_dr_)*cos(gamma_vector_[i]));
+           points2track_.push_back(Point2f((float)x, (float)y));
+        }
+    }
+
+}
 
 void OpticFlowNode::imageCb(const sensor_msgs::ImageConstPtr& image_msg){
 
     // Convert the ros image msg into a cv mat
+    image_timestamp_ = image_msg->header.stamp;
     CvImagePtr image_ptr;
     try
     {
@@ -94,20 +99,52 @@ void OpticFlowNode::imageCb(const sensor_msgs::ImageConstPtr& image_msg){
     if (init_){
         init_ = false;
         prev_grey_image_ = grey_image;
+        dt_ = 10000;
+    } else {
+        dt_ = (image_timestamp_ - last_image_timestamp_).toSec();
     }
+    last_image_timestamp_ = image_timestamp_;
 
     vector<uchar> status;
     vector<float> err;
-    cv::Size winSize(30,30);
+    cv::Size winSize(win_size_,win_size_);
     TermCriteria termcrit(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
 
     cv::calcOpticalFlowPyrLK(prev_grey_image_, grey_image, points2track_, newpoints_, status, err, winSize, 3, termcrit, 0, 0.001);
 
     // Process the CV mat and compute optic optic flow at various pixels
-    for(int i= 1; i < num_ring_points_-1; i++){
+    for (int i= 1; i < num_ring_points_*num_rings_; i++){
         cv::line(image_ptr->image, points2track_[i], newpoints_[i], CV_RGB(255,0,0),2);
     }
     prev_grey_image_ = grey_image;
+
+    // Convert flow ring points in u-v coordinates to tangential optic flow
+    u_flow_.clear();
+    v_flow_.clear();
+    for (int i = 0; i < num_ring_points_*num_rings_; i++){
+        u_flow_.push_back((points2track_[i].x - newpoints_[i].x)*pixel_scale_/dt_);
+        v_flow_.push_back(points2track_[i].y - newpoints_[i].y);
+        //ROS_INFO("%f, %f", u_flow_[i], v_flow_[i]);
+    }
+    // Convert u-v flow to tangential flow
+    int index;
+    tang_flow_.clear();
+    for (int r = 0; r < num_rings_; r++){
+        for (int i = 0; i < num_ring_points_; i++){
+            index = r*num_ring_points_ + i;
+            tang_flow_.push_back(u_flow_[index]*cos(gamma_vector_[i]) + v_flow_[i]*sin(gamma_vector_[i]));
+        }
+    }
+
+    // Compute the spatial average
+    std_msgs::Float32MultiArray tang_flow_msg;
+    ave_tang_flow_.clear();
+    for (int i = 0; i < num_ring_points_; i++){
+        ave_tang_flow_.push_back(tang_flow_[i]/num_rings_);
+    }
+    tang_flow_msg.data = ave_tang_flow_;
+
+    pub_tang_flow_.publish(tang_flow_msg);
 
     // Display the image with resulting flow
     if(debug_){
